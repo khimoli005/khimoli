bcfdc94a23182263fffb05060d92cfc4956e59fa
khimoli005 <dangalikhimoli@gmail.com>
2025-02-19 22:09:51 +0545

    include

---
+++ includes/navbar.php
@@ -0,0 +1,20 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">KHIM OLI</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="http://localhost/khimoli/aboutus.php" class="active">About</a></li>
+          <li><a href="http://localhost/khimoli/services.php">Services</a></li>
+          <li><a href="http://localhost/khimoli/portfolio.php">Portfolio</a></li>
+          <li><a href="http://localhost/khimoli/contact.php">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
